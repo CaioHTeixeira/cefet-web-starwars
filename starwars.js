b1202a41ca544217c6d7ab4f21fac8e8ebc32e64
CaioHTeixeira <caioteixeiraaa@gmail.com>
2021-12-28 23:39:09 -0300

    exercicio 0 e 1

--- starwars.js
+++ starwars.js
@@ -4,4 +4,16 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from './music.js'
+
 const API_ENDPOINT = 'https://swapi.dev/api'
+const musicInfo = {
+    audioUrl: 'audio/tema-sw.mp3', 
+    coverImageUrl: 'imgs/logo.svg', 
+    title: 'Intro', 
+    artist: 'John Williams'
+};
+
+play(musicInfo, document.body)
+
+//{audioUrl, coverImageUrl, title, artist}, parentEl
